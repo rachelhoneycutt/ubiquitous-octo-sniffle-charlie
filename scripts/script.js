7d2dbe61c00d8161d04d7adb7ab9aa6152be0dc9
rachelhoneycutt <142539235+rachelhoneycutt@users.noreply.github.com>
2023-09-07 14:26:12 -0500

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -2,7 +2,7 @@ $( document ).ready(function() {
   
   $("#DogePic").click(function(){
     if($('#DogePic').position().left > 300){
-      ($"#gameOutput").value("YOU WIN!!!");
+      alert("YOU WIN!!!");
     }
   });
   
