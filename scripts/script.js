832255901264204c4524c84947125314dbe3e8af
rachelhoneycutt <142539235+rachelhoneycutt@users.noreply.github.com>
2023-09-11 22:05:52 -0500

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,11 @@
 $( document ).ready(function() {
   
   $("#DogePic").click(function(){
-    if($('#DogePic').position().left > 300){
-      //Fixed for Charlie
-      $("#gameOutput").html("YOU WIN!!!");
+    if($('#DogePic').position().left > 300 && $('#DogePic').position().top > 300){
+      //Fixed for Charlie, outputs instead of using alert and changed postion so you can read output
+      $("#gameOutput").html("YOU WIN!!!"); 
     }
   });
   
   $( "#DogePic").draggable()
-
 });
