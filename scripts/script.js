270cc51663f05dd4f22bac27b6001b15f33ff0aa
rachelhoneycutt <142539235+rachelhoneycutt@users.noreply.github.com>
2023-09-11 22:09:46 -0500

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -2,7 +2,7 @@ $( document ).ready(function() {
   
   $("#DogePic").click(function(){
     if($('#DogePic').position().left > 300 && $('#DogePic').position().top > 300){
-      //Fixed for Charlie, outputs instead of using alert and changed postion so you can read output
+      // Fixed for Charlie, outputs instead of using alert and changed postion so you can read output
       $("#gameOutput").html("YOU WIN!!!"); 
     }
   });
