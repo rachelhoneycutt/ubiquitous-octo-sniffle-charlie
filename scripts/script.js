a1cb5f47657c908fd575a2b086e4e974dd00a672
rachelhoneycutt <142539235+rachelhoneycutt@users.noreply.github.com>
2023-09-25 23:12:42 -0500

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -3,6 +3,7 @@ $( document ).ready(function() {
   $("#DogePic").click(function(){
     if($('#DogePic').position().left > 300 && $('#DogePic').position().top > 300){
       // Fixed for Charlie, outputs instead of using alert and changed postion so you can read output
+      $("#DogePic").hide();
       $("#gameOutput").html("YOU WIN!!!"); 
     }
   });
